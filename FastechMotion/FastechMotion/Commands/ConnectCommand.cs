f55d7e123ce18d9b6446677c184cd11af09d7b9a
Nguyễn Huy Ngọ <94738497+huyngo2002@users.noreply.github.com>
2024-05-22 13:37:55 +0700

    feat: add connect ip

---
+++ FastechMotion/FastechMotion/Commands/ConnectCommand.cs
@@ -0,0 +1,40 @@
+﻿using FASTECH;
+using FastechMotion.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FastechMotion.Commands
+{
+    public class ConnectCommand : ICommand
+    {
+        private MainViewModel mainViewModel;
+        public ConnectCommand(MainViewModel mainViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            bool result = EziMOTIONPlusELib.FAS_ConnectTCP(IPAddress.Parse(mainViewModel.StrIP), mainViewModel.BoardId);
+            if (result == false)
+            {
+                MessageBox.Show($"Connect to {mainViewModel.StrIP} failed!");
+                return;
+            } 
+            MessageBox.Show($"Connect to {mainViewModel.StrIP} successed!");
+        }
+    }
+}
--- FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
+++ FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using FastechMotion.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FastechMotion.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public ViewModelBase SingleMoveCurrentView { get; set; } = new SingleMoveViewModel();
-        public ViewModelBase PositionStatusCurrentView { get; set; } = new PositionStatusViewModel();
-        public ViewModelBase JogMoveViewModel { get; set; } = new JogMoveViewModel();
-        public ViewModelBase OriginViewModel { get; set; } = new OriginViewModel();
-        public ViewModelBase FunctionViewModel { get; set; } = new FunctionViewModel();
+        public ViewModelBase SingleMoveCurrentView { get; set; } 
+        public ViewModelBase PositionStatusCurrentView { get; set; } 
+        public ViewModelBase JogMoveCurrentView { get; set; }
+        public ViewModelBase OriginCurrentView { get; set; } 
+        public ViewModelBase FunctioCurrentView { get; set; } 
+
+        private string _strIP;
+        public string StrIP
+        {
+            get { return _strIP; }
+            set { _strIP = value; OnPropertyChanged(nameof(StrIP)); }
+        }
+        private int _boardId = 8;
+        public int BoardId
+        {
+            get { return _boardId; }
+            set { _boardId = value; OnPropertyChanged(nameof(BoardId)); }   
+        }
+        public ICommand ConnectCommand { get; set; }
+        public MainViewModel()
+        {
+            SingleMoveCurrentView = new SingleMoveViewModel();
+            PositionStatusCurrentView = new PositionStatusViewModel();
+            JogMoveCurrentView = new JogMoveViewModel();
+            OriginCurrentView = new OriginViewModel();
+            FunctioCurrentView = new FunctionViewModel();
+
+            StrIP = "192.168.0.8";
+            ConnectCommand = new ConnectCommand(this);
+        }
     }
 }
