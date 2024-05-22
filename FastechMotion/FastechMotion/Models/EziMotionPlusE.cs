105cb9f101df630ceb9f7b12fdc21891c7c3bb39
Xuanhv21 <xuanhoangvinh01@gmail.com>
2024-05-22 15:11:11 +0700

    Add EziMotionPlusE

---
+++ FastechMotion/FastechMotion/Models/EziMotionPlusE.cs
@@ -0,0 +1,64 @@
+﻿using FASTECH;
+using FastechMotion.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastechMotion.Models
+{
+    public class EziMotionPlusE : ViewModelBase
+    {
+		private string _name;
+        private string _strIp;
+
+        public string Name
+		{
+			get 
+			{
+				return _name; 
+			}
+			set 
+			{
+				_name = value; 
+				OnPropertyChanged("Name");
+			}
+		}
+		public string StrIP
+		{
+			get 
+			{
+				return _strIp; 
+			}
+			set 
+			{
+                _strIp = value;
+				OnPropertyChanged("IP");
+			}
+		}
+		public int BoardID { get; set; } = 8;
+		public EziMotionPlusE(string name,string strIP) 
+		{
+			Name = name;
+			StrIP = strIP;
+			IPAddress ip = IPAddress.Parse(strIP);
+			BoardID = ip.GetAddressBytes()[3];
+		}
+
+		public void Connect()
+		{
+            EziMOTIONPlusELib.FAS_Connect(IPAddress.Parse(StrIP),BoardID);
+        }
+		public void Disconnect() 
+		{
+            EziMOTIONPlusELib.FAS_Close(BoardID);
+        }
+
+		public void MoveAbs(int absPos,uint velocity)
+		{
+			EziMOTIONPlusELib.FAS_MoveSingleAxisAbsPos(BoardID, absPos, velocity);
+        }
+	}
+}
--- FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
+++ FastechMotion/FastechMotion/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FastechMotion.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,5 +14,11 @@ namespace FastechMotion.ViewModels
         public ViewModelBase JogMoveViewModel { get; set; } = new JogMoveViewModel();
         public ViewModelBase OriginViewModel { get; set; } = new OriginViewModel();
         public ViewModelBase FunctionViewModel { get; set; } = new FunctionViewModel();
+
+        public EziMotionPlusE EziMotionDemo { get; set; }
+        public MainViewModel() 
+        {
+            EziMotionDemo = new EziMotionPlusE("Demo", "192.168.0.8");
+        }
     }
 }
